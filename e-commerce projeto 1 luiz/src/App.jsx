f6c7475e1fba3d138cb2d30ec5f56a190c60db5c
LuizGustavoViana <vluisgustavo346@gmail.com>
2026-03-29 15:16:25 -0300

    projeto base do e-commerce

---
+++ e-commerce projeto 1 luiz/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>ola mundo</h1>
+     <h1>Prontos para o projeto?</h1>
+    </>
+  )
+}
+
+export default App
