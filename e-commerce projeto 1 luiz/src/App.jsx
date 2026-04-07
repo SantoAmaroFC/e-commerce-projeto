b5b32740c804fd5fbfc081b56d4d9323bec6a29b
LuizGustavoViana <vluisgustavo346@gmail.com>
2026-04-07 16:10:56 -0300

    atualizaçao do e commerce

--- e-commerce projeto 1 luiz/src/App.jsx
+++ e-commerce projeto 1 luiz/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 
 import './App.css'
+import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
 
 function App() {
- 
+ const[tela, setTela] = useState(<Home />)
 
   return (
-    <>
-     <h1>ola mundo</h1>
-     <h1>Prontos para o projeto?</h1>
-    </>
+    <div className={'cont-app'}>
+      <header className={'cont-header'}>
+    <nav>
+    <button className={'botoes-nav'} onClick={() => setTela(<Home />)}>Home</button>
+    <button className={'botoes-nav'} onClick={() => setTela(<Cadastro />)}>Cadastro</button>
+    </nav>
+      </header>
+    <main className={'cont-main'}>
+    {tela}
+      </main> 
+    </div>
   )
 }
 
---
+++ e-commerce projeto 1 luiz/src/pages/Cadastro.jsx
@@ -0,0 +1,15 @@
+import './Paginas.css'
+
+function Cadastro() {
+  return (
+    <div className={'cont-cadastro'}>
+        <h1> Faça seu cadastro</h1>
+            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            </p>
+            
+    </div>
+  )
+}
+
+export default Cadastro
