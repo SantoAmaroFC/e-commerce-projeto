0f61c378f711dd9c931b47acc0d98687be85006f
SantoAmaroFC <luiz_g_viana@estudante.sesisenai.org.br>
2026-04-22 22:22:55 -0300

    atualizaçao foda

--- e-commerce projeto 1 luiz/src/components/Cadastros.jsx
+++ e-commerce projeto 1 luiz/src/components/Cadastros.jsx
@@ -3,16 +3,63 @@ import {useState} from 'react'
 function Cadastro() {
     const [tipo, settipo] = useState("")
     const [tamanho, settamanho] = useState("")
+    const [cor, setCor] = useState("")
   return (
     <div>
         
-        <input type="text" className='tipo' placeholder='Digite o tipo'
-            onChange={(e) => settipo((e.target.value))} 
-        />
-        <input type="text" className='tipo' placeholder='Digite o tamanho'
-            onChange={(e) => settamanho((e.target.value))} 
-        />
-        <button></button>
+        
+          <label> 
+            selecione o tipo
+          </label>
+        <select
+        >
+        <option value="">Camisa</option>
+        <option value="">Calça</option>
+        <option value="">Sapato</option>
+        <option value="">Meia</option>
+        <option value="">Short</option>
+        <option value="">Chapeu</option>
+        <option value="">Cinto</option>
+        </select>
+        <label> 
+            selecione o tamanho
+          </label>
+        <select
+        >
+        <option value="">P</option>
+        <option value="">M</option>
+        <option value="">G</option>
+        <option value="">GG</option>
+        <option value="">PP</option>
+        <option value="">XGG</option>
+        <option value="">XPP</option>
+        </select>
+
+        <label> 
+            Marca
+          </label>
+        <select
+        >
+        <option value="">Nike</option>
+        <option value="">Adidas</option>
+        <option value="">Puma</option>
+        <option value="">Lacoste</option>
+        <option value="">louis vuitton</option>
+        <option value="">Balenciaga</option>
+        
+        </select>
+        <label> 
+            Genero
+          </label>
+        <select
+        >
+        <option value="">M</option>
+        <option value="">F</option>
+       
+        
+        </select>
+        
+        
     </div>
   )
 }
