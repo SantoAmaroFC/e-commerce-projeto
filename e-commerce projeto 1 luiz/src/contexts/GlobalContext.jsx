7d77872f0d5f436153127e34af161c9b1ed8c113
SantoAmaroFC <luiz_g_viana@estudante.sesisenai.org.br>
2026-05-28 20:16:05 -0300

    aaaa

---
+++ e-commerce projeto 1 luiz/src/contexts/GlobalContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState} from "react";
+
+export const GlobalContext = createContext()
+
+export const GlobalContextProvider = ({children}) => {
+// aqui cria as infos salvas no contexto
+                        
+    // const[usuario, setUsuario] = useState("")
+    
+
+    return(
+        <GlobalContext.Provider value={{
+                // usuario, setUsuario,
+                
+            }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
