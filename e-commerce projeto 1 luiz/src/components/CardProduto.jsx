a198b7bb2d6f665d78a1dcb95b42c9f599650ae0
LUAN CARLOS JOLLEMBECK <luan_jollembeck@estudante.sesisenai.org.br>
2026-06-18 21:10:36 -0300

    atualizando

--- e-commerce projeto 1 luiz/src/components/CardProduto.jsx
+++ e-commerce projeto 1 luiz/src/components/CardProduto.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import './CardProduto.css'
+import axios from "axios"
+import { useState, useEffect } from 'react'
 
 function CardProduto(prop) {
 
+    const [editando, setEditando] = useState(false);
+
+    const deletarProduto = async (id) => {
+        try {
+            const response = await axios.delete(`http://localhost:3000/produto/${id}`);
+            if (response.status === 200) {
+                prop.fetchProdutos();
+            }
+        } catch (error) {
+            console.error('Erro ao deletar produto:', error);
+        }
+    };
+
+    // const buscarClientePorId = async (id) => {
+    //     try {
+    //         const response = await axios.get(`http://localhost:Produto/${id}`);
+    //         setClienteSelecionado(response.data);
+    //         exibirCliente(response.data);
+    //     } catch (error) {
+    //         console.error('Erro ao buscar cliente por ID:', error);
+    //     }
+    // };
+
+
     return (
-        <div className = 'card-Produtos'>
-            <p>Nome: {prop.p.nome}</p>
-            <p>Tipo: {prop.p.tipo}</p>
-            <p>tamanho: {prop.p.tamanho}</p>
-            <p>cor: {prop.p.cor}</p>
-            <p>descricao: {prop.p.descricao}</p>
-            <p>preco: {prop.p.preco}</p>
-            <p>quantidade: {prop.p.quantidade}</p>
-            <button onClick={() => excluir(id)}>excluir</button>
+        <div className='card-Produtos'>
+                <p>Nome: {prop.p.nome}</p>
+                <p>Tipo: {prop.p.tipo}</p>
+                <p>tamanho: {prop.p.tamanho}</p>
+                <p>cor: {prop.p.cor}</p>
+                <p>descricao: {prop.p.descricao}</p>
+                <p>preco: {prop.p.preco}</p>
+                <p>quantidade: {prop.p.quantidade}</p>
+                <button onClick={() => deletarProduto(prop.p.id)}>excluir</button>
+                <button onClick={() => setEditando(true)}>editar</button>
         </div>
     )
 }
